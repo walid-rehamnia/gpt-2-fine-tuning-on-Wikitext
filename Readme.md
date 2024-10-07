e06d0bb6b70817cd418c13a395284167ac983972
Walid Rehamnia <rehamniawalid.info@gmail.com>
2024-10-07 09:06:15 +0300

    - Calculating BLEU with evaluate package of hugging faces instead of sacrebleu package. - Adding several metrics - Code refactoring - Readme.md refactor

--- Readme.md
+++ Readme.md
@@ -1,46 +1,40 @@
+# Fine-Tuning GPT-2 on WikiText with Full Tuning and LoRA: A Comparative Study
+
 ## Task objective
-The objective from this task is to fine tune the "GPT-2" LLM model on Wikitext dataset with simple fine tuning as well as with LoRA technique.
-For simplicity and demonstrative purposes the code is implemented 
+The objective from this task is to fine tune the "GPT-2" LLM model on Wikitext dataset with simple fine tuning as well as with LoRA technique... (see more details on the attached PDF report)
+
+## Dataset Specification
+
+| **Dataset Name**        | **Train Set** | **Validation Set** | **Test Set** |
+|-------------------------|---------------|--------------------|--------------|
+| wikitext-2-raw-v1      | 36,718        | 3,760              | 4,358        |
 
 
+## Comparison of Fine-Tuning Approaches
 
-
-
-
-name	               train	   validation	test
-wikitext-2-raw-v1	   36718	   3760	      4358
-
-
-## GPT-2 Fine-Tuning Comparison
-
-This project compares the full fine-tuning and LoRA fine-tuning of the GPT-2 model on the WikiText dataset. Below is the comparison of key metrics:
-
-| Metric              | Full Fine-Tuning | LoRA Fine-Tuning |
-|---------------------|------------------|------------------|
-| BLEU Score          | X.XXX            | Y.YYY            |
-| Training Time (s)   | 3236.96 seconds  | 2851.43 seconds  |
-| Memory Usage (MB)   | XXX MB           | YYY MB           |
-| Trainable params    | 124,439,808      | 405,504          |
-| Best training loss  | 0.3204           | 0.3761           |
-| Best validation loss| 0.4386           | 0.4451           |
-
-Bleu of lora 435 examples: 
-BLEU score: BLEU = 35.36 75.0/33.3/25.0/25.0 (BP = 1.000 ratio = 1.000 hyp_len = 4 ref_len = 4)
-
-Bleu of full 435 examples: 
-BLEU score: BLEU = 0.00 50.0/50.0/0.0/0.0 (BP = 1.000 ratio = 1.000 hyp_len = 2 ref_len = 2)
-
-### Code
-The pipeline for both fine-tuning methods can be found in the `Full Model Fine-Tuning.ipynb` and `LoRA Model Fine-Tuning.ipynb` files.
-
+| **Metric**                            | **Full Fine-Tuning** | **LoRA Fine-Tuning** |
+|---------------------------------------|----------------------|----------------------|
+| **Model size (MB)**                   | 479                  | 6.15                 |
+| **Trainable parameters**              | 124,439,808          | 405,504              |
+| **Training Time (s)**                 | 3,694.07             | 2,851.43             |
+| **GPU memory usage (MB)**             | 975.40               | 19.34                |
+| **CPU memory usage (MB)**             | 257.81               | 98.36                |
+| **Best training loss**                | 0.3004               | 0.3706               |
+| **Best validation loss**              | 0.4379               | 0.4443               |
+| **BLEU score**                        | 0.02                 | 0.01                 |
+| **SacreBLEU score**                   | 1.36                 | 1.33                 |
+| **ROUGE-1 / ROUGE-2 / ROUGE-L**       | 0.18 / 0.02 / 0.12   | 0.17 / 0.02 / 0.11   |
+| **TER score**                         | 91.66                | 93.06                |
+| **CHRF score**                        | 17.76                | 16.56                |
+| **METEOR score**                      | -                    | -                    |
 
 
 ## Reproducibility instrcutions:
 
 1. Clone the repository:
    ```bash
-   git clone https://github.com/your-repo/gpt2-finetuning-comparison.git
-   cd gpt2-finetuning-comparison
+   git clone https://github.com/walid-rehamnia/gpt-2-fine-tuning-on-Wikitext
+   cd gpt-2-fine-tuning-on-Wikitext
    ```
 
 2. Create and activate virtual environment:
@@ -69,7 +63,9 @@ The pipeline for both fine-tuning methods can be found in the `Full Model Fine-T
    pip install --index-url https://test.pypi.org/simple/ pymeteor
    ```
 
-4. Run the fine tuning:
+### Fine tuning Pipelines (code):
+The pipeline for both fine-tuning aproaches are "full_finetuning.py" and "lora_finetuning.py" for full and LoRA tuning respectively, the entry point is the main.py where you can start the preferred aproach  with additional hyperparameter via the arguments as follows:
+
 - Run the fine tune of your choice by passing the required mode arg ("full" or "lora"),
   you can also pass the next optional args: 
    - batch_size (int): Batch size for training phase (default: 4)
@@ -86,48 +82,26 @@ The pipeline for both fine-tuning methods can be found in the `Full Model Fine-T
    ```
 
 
-minimal run
-   ```bash
-   python main.py lora --batch_size 4 --data_fraction 0.01 --epochs 2 --lora_rank 2
-   ```
-
-python main.py lora --batch_size 4 --data_fraction 0.1 --epochs 2 --lora_rank 4
-trainable params: 405,504 || all params: 124,845,312 || trainable%: 0.3248
-
-Training time: 2851.43 seconds
-GPU memory used: 19.34 MB
-CPU memory used: -84.88 MB
-#########################Losses###################
-[3.3447, 0.4993, 0.5217, 0.5506, 0.4732, 0.5142, 0.3847, 0.5568, 0.4541, 0.3877, 0.5131, 0.3716, 0.5211, 0.3769, 0.5216, 0.4257, 0.4479, 0.4955, 0.4415, 0.4635, 0.5608, 0.5376, 0.4882, 0.4685, 0.3761, 0.3769, 0.4271, 0.4146, 0.44, 0.3655, 0.3786, 0.4613, 0.466, 0.5273, 0.414, 0.4739]
-[0.5106523633003235, 0.49505043029785156, 0.486192911863327, 0.4795791804790497, 0.4752623438835144, 0.47098439931869507, 0.4683157801628113, 0.46570006012916565, 0.4630976617336273, 0.4613170921802521, 0.4592796564102173, 0.4576399028301239, 0.45664435625076294, 0.4555965065956116, 0.45435577630996704, 0.4532112777233124, 0.4525558054447174, 0.4516589343547821, 0.45068058371543884, 0.4501141905784607, 0.44942522048950195, 0.44873133301734924, 0.4479249119758606, 0.4477959871292114, 0.4474676847457886, 0.4468206763267517, 0.4466492533683777, 0.446226567029953, 0.44606468081474304, 0.4459221065044403, 0.4455977976322174, 0.44541794061660767, 0.44537782669067383, 0.4451855719089508, 0.4451984763145447, 0.4451241195201874]
-Save the model...
-
-python main.py full --batch_size 4 --data_fraction 0.1 --epochs 2
-trainable params: 124439808 || all params: 124439808 || trainable%: 100.0
-Training time: 3236.96 seconds
-GPU memory used: 975.40 MB
-CPU memory used: 257.81 MB
-Save the model...
-loss:0.32 / 'eval_loss': 0.43
-
-
-
-
-
-
-python main.py full --batch_size 4 --data_fraction 0.1 --epochs 2
-
-Training time: 3694.07 seconds
-GPU memory used: 975.40 MB
-CPU memory used: -24.15 MB
-#########################Losses###################
-[2.1174, 0.4466, 0.4769, 0.512, 0.4427, 0.4835, 0.3608, 0.5305, 0.4281, 0.3683, 0.4886, 0.3509, 0.5, 0.3603, 0.4988, 0.4028, 0.4319, 0.4737, 0.3784, 0.3784, 0.4624, 0.4441, 0.3998, 0.3862, 0.3079, 0.3108, 0.3543, 0.3474, 0.3612, 0.3004, 0.313, 0.38, 0.3883, 0.434, 0.3398, 0.3912]
-[0.46279191970825195, 0.4546510875225067, 0.45132365822792053, 0.44865044951438904, 0.4493260979652405, 0.4471680521965027, 0.44842642545700073, 0.445149689912796, 0.44527724385261536, 0.44442644715309143, 0.4451478123664856, 0.4422377347946167, 0.44156062602996826, 0.44128841161727905, 0.44036415219306946, 0.43953973054885864, 0.4393884241580963, 0.437975138425827, 0.44524630904197693, 0.4449363350868225, 0.44536733627319336, 0.4455376863479614, 0.4453529119491577, 0.44572919607162476, 0.4451650381088257, 0.44514092803001404, 0.44500768184661865, 0.4445052444934845, 0.44450056552886963, 0.4453565180301666, 0.44525739550590515, 0.4443413019180298, 0.44417569041252136, 0.4440059959888458, 0.4436773359775543, 0.4435123801231384]
-
-
-5. Check the comparison results:
+5. Code style commands:
    After both scripts finish running, a summary table with BLEU scores, training time, and memory usage will be printed.
-
+```bash
 pylint main.py full_finetuning.py lora_finetuning.py utils.py
+```
+```bash
 flake8 main.py full_finetuning.py lora_finetuning.py utils.py
+```
+```bash
 isort main.py full_finetuning.py lora_finetuning.py utils.py
+```
+```bash
+ruff check . --fix
+```
+
+
+```diff
+- Please read the attached PDF report for more details
+```
+
+```diff
++ feel free to contact me for any question
+```
--- utils.py
+++ utils.py
@@ -15,17 +15,17 @@ Functions:
 Author: [r.walid]
 """
 
-
 import time
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import psutil
-import pymeteor.pymeteor as pymeteor
+# import pymeteor.pymeteor as pymeteor
 import sacrebleu
 import torch
+import os
 from datasets import DatasetDict, load_dataset
 from tqdm import tqdm
+import evaluate
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainerCallback
 
 SEED = 2024
@@ -34,6 +34,17 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class TimerMemoryTracker:
+    """
+    Context manager class to track the time and memory consumption (both GPU and CPU)
+    during model training or inference processes.
+    It measures the elapsed time and the memory used on both CPU and GPU, if available.
+
+    Methods:
+        __enter__(): Starts the timer and memory tracking.
+        __exit__(): Stops the timer and memory tracking.
+        report(): Prints the elapsed time and memory consumption details.
+    """
+
     def __init__(self):
         self.start_time = None
         self.end_time = None
@@ -67,6 +78,9 @@ class TimerMemoryTracker:
         self.end_cpu_memory = psutil.Process().memory_info().rss
 
     def report(self):
+        """
+        Prints the elapsed time, GPU memory usage, and CPU memory usage.
+        """
         # Report time and memory consumption
         elapsed_time = self.end_time - self.start_time
         gpu_memory_used = self.end_gpu_memory - \
@@ -79,6 +93,14 @@ class TimerMemoryTracker:
 
 
 class LossRecorderCallback(TrainerCallback):
+    """
+    A callback class to record training and evaluation loss values during
+    the model's training and evaluation process.
+
+    Methods:
+        on_log(): Appends the loss values from the logs into the appropriate lists.
+    """
+
     def __init__(self):
         # Initialize lists to store losses
         self.train_losses = []
@@ -95,6 +117,13 @@ class LossRecorderCallback(TrainerCallback):
 
 
 def plot_losses(train_losses, eval_losses):
+    """
+    Plots the training and validation loss over epochs for visualization purposes.
+
+    Args:
+        train_losses (list): List of training loss values.
+        eval_losses (list): List of evaluation loss values.
+    """
     plt.figure(figsize=(10, 6))
     plt.plot(train_losses, label='Training Loss')
     plt.plot(eval_losses, label='Validation Loss')
@@ -107,9 +136,12 @@ def plot_losses(train_losses, eval_losses):
 
 
 class TokenizerSingleton:
-    """Asengloten class which ensures that only one instance of the tokenizer
-      is generated in the whole project ( for better performance )
+    """
+    Singleton class that ensures a single instance of the GPT-2 tokenizer
+    is created and reused throughout the application to optimize performance.
 
+    Methods:
+        get_tokenizer(): Returns the single instance of the GPT-2 tokenizer.
     """
     _instance = None
 
@@ -123,19 +155,44 @@ class TokenizerSingleton:
         return cls._instance
 
     def get_tokenizer(self):
+        """
+        Returns the singleton GPT-2 tokenizer instance.
+        """
         return self.tokenizer
 
 
 def get_model():
+    """
+    Loads and returns a pre-trained GPT-2 model with automatic device mapping.
+
+    Returns:
+        model: Pre-trained GPT-2 model.
+    """
     return AutoModelForCausalLM.from_pretrained("gpt2", device_map='auto')
 
 
 def freeze_weights(model) -> None:
+    """
+    Freezes all the weights in the given model to prevent further training.
+
+    Args:
+        model (torch.nn.Module): The model to freeze weights for.
+    """
     for param in model.parameters():
         param.requires_grad = False
 
 
 def load_data(fraction=1):
+    """
+    Loads the WikiText dataset and returns it as a DatasetDict object.
+    Can return a subset of the data based on the provided fraction.
+
+    Args:
+        fraction (float): Fraction of the data to load (between 0 and 1).
+
+    Returns:
+        DatasetDict: A dictionary with train, validation, and test datasets.
+    """
     dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
 
     # If a valid fraction is passed, return a fraction of the data
@@ -153,6 +210,15 @@ def load_data(fraction=1):
 
 
 def load_test_data(num_examples: int):
+    """
+    Loads a subset of test examples from the WikiText dataset.
+
+    Args:
+        num_examples (int): The number of test examples to load.
+
+    Returns:
+        list: List of test examples.
+    """
     # Step 1: Load the test dataset
     test_dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
     # remove the void examples
@@ -167,6 +233,15 @@ def load_test_data(num_examples: int):
 
 
 def tokenize_function(examples):
+    """
+    Tokenizes the given examples using the GPT-2 tokenizer.
+
+    Args:
+        examples (dict): A dictionary of text examples to tokenize.
+
+    Returns:
+        dict: Tokenized examples with input_ids, attention_mask, and labels.
+    """
     tokenizer = TokenizerSingleton().get_tokenizer()
     # 1024 is set as the max_length to utilizes the full context
     #  window of GPT-2, which is better for understanding long sequences
@@ -179,6 +254,15 @@ def tokenize_function(examples):
 
 
 def get_tokenized_dataset(dataset):
+    """
+    Tokenizes the given dataset and formats it for PyTorch models.
+
+    Args:
+        dataset (DatasetDict): The dataset to tokenize.
+
+    Returns:
+        DatasetDict: The tokenized and formatted dataset.
+    """
     tokenized_dataset = dataset.map(
         tokenize_function, batched=True,  remove_columns=["text"])
     tokenized_dataset.set_format(
@@ -189,6 +273,9 @@ def get_tokenized_dataset(dataset):
 def print_trainable_parameters(model):
     """
     Prints the number of trainable parameters in the model.
+
+    Args:
+        model (torch.nn.Module): The model to analyze.
     """
     trainable_params = 0
     all_param = 0
@@ -205,6 +292,22 @@ def print_trainable_parameters(model):
 # Function to generate text
 def generate_text(model: AutoModelForCausalLM, tokenizer: AutoTokenizer,
                   prompt: str, min_length: int, max_length: int):
+    """
+    Generates text based on a given prompt using a specified model and tokenizer.
+
+    Args:
+        model (AutoModelForCausalLM): The pre-trained language model (e.g., GPT-2)
+        for generating text.
+        tokenizer (AutoTokenizer): The tokenizer corresponding to the model
+        for encoding the input prompt.
+        prompt (str): The initial text prompt to guide text generation.
+        min_length (int): Minimum number of tokens to generate.
+        max_length (int): Maximum number of tokens to generate.
+
+    Returns:
+        str: The generated text based on the given prompt, decoded and stripped
+        of special tokens.
+    """
     inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
     outputs = model.generate(
         inputs["input_ids"], min_length=min_length, max_length=max_length,
@@ -220,6 +323,22 @@ def generate_text(model: AutoModelForCausalLM, tokenizer: AutoTokenizer,
 
 
 def generate_model_hypothesis_references(model_path, examples):
+    """
+    Generates hypotheses and references for a list of examples
+    using a pre-trained model.
+
+    Args:
+        model_path (str): The path to the fine-tuned model.
+        examples (list of str): A list of text examples,
+        where each example will be split intotwo parts,
+        used to generate the hypothesis and reference.
+
+    Returns:
+        tuple: 
+            - hypotheses (list of str): The generated hypotheses (model predictions).
+            - references (list of list of str): The true references corresponding
+            to the second part of the examples.
+    """
     # Step 1: Load the fine-tuned model and tokenizer
     model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto')
     # tokenizer = TokenizerSingleton().get_tokenizer()
@@ -257,6 +376,23 @@ def generate_model_hypothesis_references(model_path, examples):
 
 
 def evaluate_model(model_path, num_examples, file_path):
+    """
+    Evaluates a model using a subset of examples by calculating
+    different metrics after preparing the base tabular data.
+
+    Args:
+        model_path (str): The path to the model being evaluated.
+        num_examples (int): Number of examples to load for evaluation.
+        file_path (str): The path where evaluation results (hypotheses, references)
+        will be saved as a CSV file.
+
+    Returns:
+        None
+    """
+    # Check if the file exists
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"The file at {file_path} was not found.")
+    
     # Step 1: Load the test data
     examples = load_test_data(num_examples=num_examples)
 
@@ -267,20 +403,7 @@ def evaluate_model(model_path, num_examples, file_path):
     if len(hypotheses) == 0 or len(references) == 0:
         return
 
-    # Step 3: Calculate BLEU score
-    bleu_score = sacrebleu.corpus_bleu(hypotheses, references)
-    print(f"BLEU score: {bleu_score}")
-
-    # # Step 3: Calculate ROUGE scores
-    # rouge = Rouge()
-    # rouge_scores = rouge.get_scores(hypotheses, references)
-
-    # # Display the ROUGE scores
-    # for i, score in enumerate(rouge_scores):
-    #     print(f"Generated Text {i + 1}:")
-    #     print(score)
-
-    # Step 4:Save each example with its hypothesis and reference;
+    # Step 3:Save each example with its hypothesis and reference;
     # for comparaison, post-processing, ...
     data = {
         "examples": examples,
@@ -290,9 +413,22 @@ def evaluate_model(model_path, num_examples, file_path):
     df = pd.DataFrame(data)
     df.to_csv(file_path)
 
+    calculate_metrics(file_path=file_path)
+
 
 def evaluate_across_models(full_model_path, lora_model_path, num_examples):
+    """
+    Evaluates two models (full fine-tuned and LoRA fine-tuned)
+    and compares their BLEU scores.
 
+    Args:
+        full_model_path (str): Path to the fully fine-tuned model.
+        lora_model_path (str): Path to the LoRA fine-tuned model.
+        num_examples (int): Number of examples to use for evaluation.
+
+    Returns:
+        None
+    """
     # Step 1: Load the test data
     examples = load_test_data(num_examples=num_examples)
 
@@ -305,17 +441,7 @@ def evaluate_across_models(full_model_path, lora_model_path, num_examples):
     lora_hypotheses, _ = generate_model_hypothesis_references(
         model_path=lora_model_path, examples=examples)
 
-    # Step 3: Calculate BLEU score
-    initial_bleu = sacrebleu.corpus_bleu(initial_hypotheses, references)
-    full_bleu = sacrebleu.corpus_bleu(full_hypotheses, references)
-    lora_bleu = sacrebleu.corpus_bleu(lora_hypotheses, references)
-
-    print("********  BLEU evaluation  *********")
-    print(f"-Initial-gpt model: {initial_bleu}")
-    print(f"-Full-tuned model : {full_bleu}")
-    print(f"-LoRA-tuned model : {lora_bleu}")
-
-    # Step 4:Save each example with its hypothesis and reference
+    # Step 2:Save each example with its hypothesis and reference
     # for comparaison, post-processing, ...
     data = {
         "examples": examples,
@@ -328,16 +454,60 @@ def evaluate_across_models(full_model_path, lora_model_path, num_examples):
     df.to_csv("results/hypothesis_references_across_models.csv")
 
 
-def calculate_pymeteor_score(file_path):
+def calculate_metrics(file_path) -> None:
+    """
+    Calculates the different metrics (bleu, sacrebleu, rouge, ter_score, 
+    chrf_score, and meteor)from the generated hypotheses and references
+    saved in a CSV file.
 
+    Args:
+        file_path (str): The path to the CSV file containing hypotheses and references.
+
+    Returns:
+        None
+    """
+    # Check if the file exists
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"The file at {file_path} was not found.")
+    
     df = pd.read_csv(file_path)
-    print(df.columns)
-    hypotheses = df['hypotheses'].values.tolist()
-    references = df['references'].values.tolist()
-    sum_scores = 0
-    length = len(references)
-    for hypothesis, reference in zip(hypotheses, references):
-        sum_scores += pymeteor.meteor(hypothesis, reference)
+    # Remove rows with any null values
+    df = df.dropna()
+    hypotheses = df['hypotheses'].tolist()
+    references = df['references'].tolist()
+    # Remove any potential unwanted brackets and quotes from references
+    references = [element.strip('[]\'')
+                  for element in references]
 
-    average_meteor_score = sum_scores/length
-    print(average_meteor_score)
+    # metrics expects the reference list to be a list of lists
+    references = [[reference] for reference in references]
+
+    bleu = evaluate.load("bleu")
+    results = bleu.compute(predictions=hypotheses, references=references)
+    print(f"BLEU:{results}")
+
+    sacrebleu1 = evaluate.load("sacrebleu")
+    results = sacrebleu1.compute(predictions=hypotheses, references=references)
+    print(f"sacrebleu:{results}")
+
+    rouge = evaluate.load("rouge")
+    results = rouge.compute(predictions=hypotheses, references=references)
+    print(f"rouge:{results}")
+
+    ter_score = 0
+    chrf_score = 0
+    # meteor_score = 0
+    length = len(references)
+    for hypothesis, reference in tqdm(zip(hypotheses[85:], references[85:]),
+                                      desc="Calculating score", unit="example"):
+        ter_score += sacrebleu.sentence_ter(hypothesis, reference).score
+        chrf_score += sacrebleu.sentence_chrf(hypothesis, reference).score
+        # meteor_score += pymeteor.meteor(hypothesis, reference[0])
+
+    # Getting the average of the different scores
+    ter_score /= length
+    chrf_score /= length
+    # meteor_score /= length
+    print(f"TER score:{ter_score:.2f}")
+    print(f"CHRF score:{chrf_score:.2f}")
+    # print(f"METEOR score:{chrf_score:.2f}")
